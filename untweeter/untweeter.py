bcbef7a7d9f4ab71eea22e9532d90a3267dd3973
agricolab <robertsadresse@gmx.de>
2019-02-14 15:45:25 +0100

    Handle try-catch better for untweeter Change language to fave from like

--- untweeter/untweeter.py
+++ untweeter/untweeter.py
@@ -15,18 +15,22 @@ def delete(api, restrict=100, args=None):
         request_count = 0
         for tweet in reversed(marks):
             try:
-                print ('Deleting tweet#', request_count, tweet.id, end='')
+                print ('Untweeting #', request_count, tweet.id, end='')
                 api.DestroyStatus(status_id=tweet.id)
-                print('...deleted!')
+                print('...untweeted!')
                 time.sleep(0.1)
                 request_count += 1
             except twitter.TwitterError as err:
-                if 'No status found with that ID.' in err.message[0]['message']:
-                    print('...not found')
+                if err.message[0]['code'] == 142:
+                    print('...account is protected')
+                elif err.message[0]['code'] == 50:
+                    print('...user not found')
+                elif err.message[0]['code'] == 144:
+                    print('...tweet not found')
                 else:
                     print("failed with: %s\n" % err.message)
     else:
-        print('No need to delete any tweets')
+        print('No need to untweet any tweets')
 
 def unlike(api, restrict=100, args=None):
     faves = api.GetFavorites(count=200, include_entities=False)
@@ -40,14 +44,20 @@ def unlike(api, restrict=100, args=None):
         request_count = 0
         for tweet in reversed(marks):
             try:
-                print ('Deleting like #', request_count, tweet.id, end='')
+                print ('Unfaving #', request_count, tweet.id, end='')
                 api.DestroyFavorite(status_id=tweet.id)
-                print('...unliked!')
+                print('...unfaved!')
                 time.sleep(0.1)
                 request_count += 1
             except twitter.TwitterError as err:
-                if 'No status found with that ID.' in err.message[0]['message']:
-                    print('...not found')
+                if err.message[0]['code'] == 142:
+                    print('...account is protected')
+                elif err.message[0]['code'] == 50:
+                    print('...user not found')
+                    api.DestroyFavorite(status_id=tweet_id)
+                    print('...unfaved!')
+                elif err.message[0]['code'] == 144:
+                    print('...tweet not found')
                 else:
                     print("failed with: %s\n" % err.message)
     else:
